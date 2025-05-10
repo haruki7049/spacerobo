f36cdfb4dcd8bf5d0ed3c2ada3b913fa28aed49a
haruki7049 <tontonkirikiri@gmail.com>
2025-05-10 19:16:54 +0900

    feat(CLI): Init clap

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use bevy::prelude::*;
+use clap::Parser;
 
 fn main() {
+    let _args: CLIArgs = CLIArgs::parse();
+
     App::new().add_plugins(DefaultPlugins).run();
 }
+
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct CLIArgs;
