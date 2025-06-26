f26471c856501c333147d2422b526d5215075231
haruki7049 <tontonkirikiri@gmail.com>
2025-06-26 20:11:46 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use bevy::{
-    color::palettes::basic::RED,
+    color::palettes::basic::BLACK,
     color::palettes::basic::SILVER,
     prelude::*,
 };
@@ -10,18 +10,12 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, window_controls)
-        .add_systems(Update, control_position)
+        .add_systems(Update, rotate_light)
         .run();
 }
 
-
-/// A marker component for our shapes so we can query them separately from the ground plane
 #[derive(Component)]
 struct Shape;
-
-/// A marker component for the camera
-#[derive(Component)]
-struct Player;
 
 fn setup(
     mut commands: Commands,
@@ -35,7 +29,7 @@ fn setup(
     for shape in extrusions.into_iter() {
         commands.spawn((
             Mesh3d(shape),
-            MeshMaterial3d(materials.add(Color::from(RED))),
+            MeshMaterial3d(materials.add(Color::from(BLACK))),
             Transform::from_xyz(0.0, 2.0, 0.0),
             Shape,
         ));
@@ -63,37 +57,7 @@ fn setup(
     commands.spawn((
         Camera3d::default(),
         Transform::from_xyz(0.0, 7., 14.0).looking_at(Vec3::new(0., 1., 0.), Vec3::Y),
-        Player,
     ));
-}
-
-fn control_position(
-    mut query: Query<&mut Transform, With<Shape>>,
-    input: Res<ButtonInput<KeyCode>>,
-) {
-    if input.pressed(KeyCode::ArrowRight) {
-        for mut transform in &mut query {
-            transform.translation.x += 0.1;
-        }
-    }
-
-    if input.pressed(KeyCode::ArrowLeft) {
-        for mut transform in &mut query {
-            transform.translation.x -= 0.1;
-        }
-    }
-
-    if input.pressed(KeyCode::ArrowDown) {
-        for mut transform in &mut query {
-            transform.translation.z += 0.1;
-        }
-    }
-
-    if input.pressed(KeyCode::ArrowUp) {
-        for mut transform in &mut query {
-            transform.translation.z -= 0.1;
-        }
-    }
 }
 
 fn window_controls(
@@ -109,5 +73,16 @@ fn window_controls(
         if input.just_pressed(KeyCode::Escape) {
             commands.entity(window).despawn();
         }
+    }
+}
+
+fn rotate_light(
+    mut query: Query<&mut Transform, With<Shape>>,
+    time: Res<Time>,
+) {
+    for mut transform in &mut query {
+        transform.rotate_x(time.delta_secs());
+        transform.rotate_y(time.delta_secs());
+        transform.rotate_z(time.delta_secs());
     }
 }
