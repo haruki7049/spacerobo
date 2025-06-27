7a0b154b0a8db15c17dc90810ccfe9d0cfb384f2
haruki7049 <tontonkirikiri@gmail.com>
2025-06-27 14:41:09 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
 use avian3d::prelude::*;
-use bevy::{color::palettes::basic::BLACK, color::palettes::basic::SILVER, prelude::*};
+use bevy::{color::palettes::basic::SILVER, prelude::*};
 
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
         .insert_resource(Gravity(Vec3::NEG_Y * 0.))
         .add_systems(Startup, setup)
-        .add_systems(Update, accelerate_angular)
+        .add_systems(Startup, spacerobo::player::setup)
+        .add_systems(Update, spacerobo::player::controller_system)
         .run();
 }
-
-#[derive(Component)]
-struct Cube;
 
 fn setup(
     mut commands: Commands,
@@ -26,20 +24,6 @@ fn setup(
         RigidBody::Static,
     ));
 
-    commands.spawn((
-        Mesh3d(meshes.add(Cuboid::default())),
-        MeshMaterial3d(materials.add(Color::from(BLACK))),
-        Transform::from_xyz(0.0, 2.0, 0.0),
-        // RigidBody
-        RigidBody::Dynamic,
-        GravityScale(0.2),
-        // Collider
-        Collider::cuboid(1.0, 1.0, 1.0),
-        // Velocity
-        AngularVelocity(Vec3::ZERO),
-        Cube,
-    ));
-
     // Light
     commands.spawn((
         PointLight {
@@ -49,23 +33,4 @@ fn setup(
         },
         Transform::from_xyz(2.0, 8.0, 2.0),
     ));
-
-    // Camera
-    commands.spawn((
-        Camera3d::default(),
-        Transform::from_xyz(-5.0, 3.5, 5.5).looking_at(Vec3::ZERO, Vec3::Y),
-    ));
-}
-
-fn accelerate_angular(
-    mut query: Query<&mut AngularVelocity, With<Cube>>,
-    keyboard: Res<ButtonInput<KeyCode>>,
-) {
-    if keyboard.just_pressed(KeyCode::Space) {
-        for mut angular_velocity in &mut query {
-            angular_velocity.x += 0.5;
-            angular_velocity.y += 0.5;
-            angular_velocity.z += 0.5;
-        }
-    }
 }
---
+++ src/player.rs
@@ -0,0 +1,40 @@
+use avian3d::prelude::*;
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Player;
+
+pub fn setup(
+    mut commands: Commands,
+) {
+    // Camera
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(-5.0, 3.5, 5.5).looking_at(Vec3::ZERO, Vec3::Y),
+        RigidBody::Dynamic,
+        GravityScale(0.2),
+        Collider::sphere(1.0),
+        AngularVelocity(Vec3::ZERO),
+        Player,
+    ));
+}
+
+pub fn controller_system(
+    mut angular_query: Query<&mut AngularVelocity, With<Player>>,
+    gamepads: Query<(Entity, &Gamepad)>,
+) {
+    for (entity, gamepad) in &gamepads {
+        let left_stick = gamepad.left_stick();
+        let right_stick = gamepad.right_stick();
+
+        info!("{} right_stick: {}", entity, right_stick);
+        info!("{} left_stick: {}", entity, left_stick);
+
+        for mut angular_velocity in &mut angular_query {
+            //let velocity_x = left_stick.x / 3.;
+            let velocity_x = 0.0 / 3.;
+
+            angular_velocity.x += velocity_x;
+        }
+    }
+}
