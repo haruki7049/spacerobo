038d2e393b4bb20defac1f14f99874679dc23bb6
haruki7049 <tontonkirikiri@gmail.com>
2025-06-27 18:59:42 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
         .insert_resource(Gravity(Vec3::NEG_Y * 0.))
         .add_systems(Startup, setup)
         .add_systems(Startup, spacerobo::player::setup)
-        .add_systems(Update, spacerobo::player::controller_system)
+        .add_systems(Update, spacerobo::player::keyboard_mouse_system)
         .run();
 }
 
--- src/player.rs
+++ src/player.rs
@@ -1,12 +1,10 @@
 use avian3d::prelude::*;
-use bevy::prelude::*;
+use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*};
 
 #[derive(Component)]
 pub struct Player;
 
-pub fn setup(
-    mut commands: Commands,
-) {
+pub fn setup(mut commands: Commands) {
     // Camera
     commands.spawn((
         Camera3d::default(),
@@ -19,6 +17,32 @@ pub fn setup(
     ));
 }
 
+pub fn keyboard_mouse_system(
+    mut angular_query: Query<&mut AngularVelocity, With<Player>>,
+    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
+    key_input: Res<ButtonInput<KeyCode>>,
+) {
+    if accumulated_mouse_motion.delta != Vec2::ZERO {
+        let delta = accumulated_mouse_motion.delta;
+
+        for mut angular_velocity in &mut angular_query {
+            let velocity_z = delta.y * -1. / 10.;
+            let velocity_y = delta.x * -1. / 10.;
+
+            angular_velocity.z += velocity_z;
+            angular_velocity.y += velocity_y;
+        }
+    }
+
+    // Hovering
+    if key_input.just_pressed(KeyCode::KeyH) {
+        for mut angular_velocity in &mut angular_query {
+            angular_velocity.z = 0.;
+            angular_velocity.y = 0.;
+        }
+    }
+}
+
 pub fn controller_system(
     mut angular_query: Query<&mut AngularVelocity, With<Player>>,
     gamepads: Query<(Entity, &Gamepad)>,
@@ -27,8 +51,8 @@ pub fn controller_system(
         let left_stick = gamepad.left_stick();
         let right_stick = gamepad.right_stick();
 
-        info!("{} right_stick: {}", entity, right_stick);
-        info!("{} left_stick: {}", entity, left_stick);
+        debug!("{} right_stick: {}", entity, right_stick);
+        debug!("{} left_stick: {}", entity, left_stick);
 
         for mut angular_velocity in &mut angular_query {
             let velocity_z = (right_stick.x * -1.0) / 3.;
