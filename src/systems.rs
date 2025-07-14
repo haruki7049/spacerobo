e3ae16987f58781bb3a39bbbf66dfd42266213de
haruki7049 <tontonkirikiri@gmail.com>
2025-07-14 20:48:55 +0900

    WIP

--- src/systems.rs
+++ src/systems.rs
@@ -8,17 +8,45 @@ use bevy::prelude::*;
 pub fn collision_detection_system(
     mut commands: Commands,
     mut collision_event_reader: EventReader<CollisionStarted>,
-    mut query: Query<(&Hp, &LinearVelocity, &Mass)>,
+    mut query: Query<(&mut Hp, &LinearVelocity, &Mass)>,
     asset_server: Res<AssetServer>,
 ) {
     for CollisionStarted(entity1, entity2) in collision_event_reader.read() {
         debug!("Collision!!");
 
-        let object1 = query.get(*entity1);
-        let object2 = query.get(*entity2);
+        let mut object1 = query.get_mut(*entity1).ok();
+        let mut object2 = query.get_mut(*entity2).ok();
 
-        dbg!(object1);
-        dbg!(object2);
-        todo!()
+        match (object1, object2) {
+            (Some(obj1), Some(obj2)) => {
+                let obj1_damage: f32 = calc_damage(obj1);
+                let obj2_damage: f32 = calc_damage(obj2);
+
+                dbg!(&obj1_damage);
+                dbg!(&obj2_damage);
+
+                let damage: f32 = obj1_damage + obj2_damage;
+
+                todo!()
+            }
+            (None, Some(obj)) | (Some(obj), None) => {
+                let damage: f32 = calc_damage(obj);
+
+                dbg!(damage);
+                todo!();
+            }
+            (None, None) => (),
+        }
     }
+}
+
+fn calc_damage(object: (Mut<'_, Hp>, &LinearVelocity, &Mass)) -> f32 {
+    let (hp, linear, mass) = object;
+
+    let speed: f32 = linear.x + linear.y + linear.z;
+
+    // Speed * Mass = Force
+    // By Isaac Newton
+    // Probably...
+    (speed * **mass).abs()
 }
