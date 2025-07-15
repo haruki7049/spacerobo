b37c10d3f2a6a01e5899474b8e12c5071e386dc3
haruki7049 <tontonkirikiri@gmail.com>
2025-07-15 23:44:47 +0900

    FIX

---
+++ src/target/health.rs
@@ -0,0 +1,21 @@
+use crate::{DeathEvent, target::Target};
+use bevy::prelude::*;
+
+pub fn update_system(
+    mut commands: Commands,
+    mut death_reader: EventReader<DeathEvent>,
+    query: Query<&Target>,
+    asset_server: Res<AssetServer>,
+) {
+    for death_event in death_reader.read() {
+        if query.get(death_event.entity).is_ok() {
+            commands.spawn((
+                AudioPlayer::new(asset_server.load("SE/kill.ogg")),
+                PlaybackSettings::ONCE.with_spatial(false),
+            ));
+
+            // Despawn the target
+            commands.entity(death_event.entity).despawn();
+        }
+    }
+}
