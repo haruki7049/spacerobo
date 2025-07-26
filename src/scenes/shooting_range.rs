cc2f24d1cab81e5097c21a268cfee9446665432d
haruki7049 <tontonkirikiri@gmail.com>
2025-07-26 18:38:53 +0900

    feat(shooting_range): Update targets' position

--- src/scenes/shooting_range.rs
+++ src/scenes/shooting_range.rs
@@ -3,7 +3,7 @@ pub mod health;
 use crate::{DeathEvent, GameMode, Hp};
 use avian3d::prelude::*;
 use bevy::{
-    color::palettes::basic::{BLACK, BLUE, GREEN, RED, SILVER, YELLOW},
+    color::palettes::basic::{BLUE, GREEN, RED, YELLOW, WHITE},
     prelude::*,
 };
 
@@ -25,32 +25,139 @@ pub fn setup_system(
         Transform::from_xyz(2.0, 8.0, 2.0),
     ));
 
-    let targets_translation: Vec<(Vec3, Color)> = vec![
-        (Vec3::new(10.0, 0.0, 0.0), RED.into()),
-        (Vec3::new(0.0, 10.0, 0.0), BLUE.into()),
-        (Vec3::new(0.0, 0.0, 10.0), GREEN.into()),
-        (Vec3::new(-10.0, 0.0, 0.0), YELLOW.into()),
-        (Vec3::new(0.0, -10.0, 0.0), SILVER.into()),
-        (Vec3::new(0.0, 0.0, -10.0), BLACK.into()),
-    ];
-
     // Targets
-    for (translation, base_color) in targets_translation {
-        commands.spawn((
-            StateScoped(GameMode::ShootingRange),
-            Mesh3d(meshes.add(Sphere::default().mesh())),
-            MeshMaterial3d(materials.add(StandardMaterial {
-                base_color,
-                ..Default::default()
-            })),
-            Transform::from_translation(translation),
-            RigidBody::Static,
-            Collider::sphere(1.0),
-            CollisionEventsEnabled,
-            Mass(1.0),
-            Target,
-            Hp::default(),
-        ));
+    for i in 1..5 {
+        for j in 1..5 {
+            for k in 1..5 {
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: RED.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(10.0 * i as f32, 10.0 * j as f32, 10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: WHITE.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(10.0 * i as f32, 10.0 * j as f32, -10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: WHITE.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(10.0 * i as f32, -10.0 * j as f32, 10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: GREEN.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(10.0 * i as f32, -10.0 * j as f32, -10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: WHITE.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(-10.0 * i as f32, 10.0 * j as f32, 10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: YELLOW.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(-10.0 * i as f32, 10.0 * j as f32, -10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: BLUE.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(-10.0 * i as f32, -10.0 * j as f32, 10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+
+                commands.spawn((
+                    StateScoped(GameMode::ShootingRange),
+                    Mesh3d(meshes.add(Sphere::default().mesh())),
+                    MeshMaterial3d(materials.add(StandardMaterial {
+                        base_color: WHITE.into(),
+                        ..Default::default()
+                    })),
+                    Transform::from_xyz(-10.0 * i as f32, -10.0 * j as f32, -10.0 * k as f32),
+                    RigidBody::Static,
+                    Collider::sphere(1.0),
+                    CollisionEventsEnabled,
+                    Mass(1.0),
+                    Target,
+                    Hp::default(),
+                ));
+            }
+        }
     }
 }
 
