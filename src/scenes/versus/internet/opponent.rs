63f0e1384b9614d9f624bdfd28ba942b150cfad8
haruki7049 <tontonkirikiri@gmail.com>
2025-08-01 19:17:08 +0900

    UpdatE

--- src/scenes/versus/internet/opponent.rs
+++ src/scenes/versus/internet/opponent.rs
@@ -1,12 +1,34 @@
 use super::PlayerInfo;
-use bevy::prelude::*;
+use crate::Hp;
+use bevy::{
+    prelude::*,
+    color::palettes::basic::RED,
+};
 use bevy_octopus::prelude::*;
+use avian3d::prelude::*;
+
+#[derive(Component)]
+struct Opponent;
 
 pub fn update_system(
-    //mut commands: Commands,
+    mut commands: Commands,
     mut channel_received: EventReader<ReceiveChannelMessage<PlayerInfo>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
 ) {
     for event in channel_received.read() {
-        info!("Received: {:?}", event);
+        commands.spawn((
+            (event.message.transform),
+            Mesh3d(meshes.add(Sphere::default().mesh())),
+            MeshMaterial3d(materials.add(StandardMaterial {
+                base_color: RED.into(),
+                ..Default::default()
+            })),
+            RigidBody::Dynamic,
+            Collider::sphere(1.0),
+            Mass(5.0),
+            Hp::player(),
+            Opponent,
+        ));
     }
 }
