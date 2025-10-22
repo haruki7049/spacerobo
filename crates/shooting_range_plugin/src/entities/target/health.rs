2224c703d86d20e3ac9ed7187940a6ddcadddb44
haruki7049 <tontonkirikiri@gmail.com>
2025-10-22 17:46:20 +0900

    fix(gitignore): Fix target directory's ignore setting

---
+++ crates/shooting_range_plugin/src/entities/target/health.rs
@@ -0,0 +1,17 @@
+use super::Target;
+use bevy::prelude::*;
+use spacerobo_commons::DeathEvent;
+
+pub fn update_system(
+    mut commands: Commands,
+    mut event_reader: EventReader<DeathEvent>,
+    query: Query<&Target>,
+) {
+    for death_event in event_reader.read() {
+        if query.get(death_event.entity()).is_ok() {
+            commands.entity(death_event.entity()).despawn();
+
+            debug!("Target is dead!!");
+        }
+    }
+}
