83b949d2b8e9b4ac26218269b0637dbb70f0f809
haruki7049 <tontonkirikiri@gmail.com>
2025-12-12 11:03:38 +0900

    style: nix fmt

--- crates/client/crates/shooting_range_plugin/src/lib.rs
+++ crates/client/crates/shooting_range_plugin/src/lib.rs
@@ -3,7 +3,10 @@ use bevy::{
     color::palettes::basic::{BLUE, GREEN, RED, WHITE, YELLOW},
     prelude::*,
 };
-use spacerobo_commons::{DeathEvent, GameMode, Hp, KillCounter, entity::{EntityPlugins, bot, target}};
+use spacerobo_commons::{
+    DeathEvent, GameMode, Hp, KillCounter,
+    entity::{EntityPlugins, bot, target},
+};
 
 pub struct ShootingRangePlugin;
 
