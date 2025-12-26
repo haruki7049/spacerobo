50f9ec50799db2dc7ad0449b461254af20bbe261
haruki7049 <tontonkirikiri@gmail.com>
2025-12-26 21:40:21 +0900

    fix(spacerobo_entity): Add kill SE for bot, player, and target

--- crates/entity/src/entity/bot/health.rs
+++ crates/entity/src/entity/bot/health.rs
@@ -5,11 +5,15 @@ use spacerobo_commons::DeathEvent;
 pub fn update_system(
     mut commands: Commands,
     mut event_reader: EventReader<DeathEvent>,
+    asset_server: Res<AssetServer>,
     query: Query<&Bot>,
 ) {
     for death_event in event_reader.read() {
         if query.get(**death_event).is_ok() {
             commands.entity(**death_event).despawn();
+            commands.spawn(AudioPlayer::new(
+                asset_server.load("SE/kill.ogg"),
+            ));
 
             debug!("Bot is dead!!");
         }
