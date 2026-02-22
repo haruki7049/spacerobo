e54d383ae46aff7639af4a3b799d6d94be515fe8
haruki7049 <tontonkirikiri@gmail.com>
2026-02-22 21:33:53 +0900

    test(spacerobo_bot_gun): Create gun::bullet::tests::common_bullet::shoot_common_bullet

--- crates/bot/gun/src/gun/bullet.rs
+++ crates/bot/gun/src/gun/bullet.rs
@@ -9,6 +9,7 @@ const BULLET_SIZE: f32 = 1. / 8.;
 pub struct Common;
 
 impl Bullet for Common {
+    /// Shoots a bullet from origin (Vec3) with a force (Vec3).
     fn shoot(
         commands: &mut Commands,
         meshes: &mut ResMut<Assets<Mesh>>,
@@ -33,11 +34,116 @@ impl Bullet for Common {
         ));
     }
 
+    /// Play gunfire sound. Pow!!
     fn gunfire_sound(commands: &mut Commands, asset_server: &Res<AssetServer>, place: Vec3) {
         commands.spawn((
             Transform::from_translation(place),
             AudioPlayer::new(asset_server.load("SE/shoot.ogg")),
             PlaybackSettings::ONCE.with_spatial(false),
         ));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    mod common_bullet {
+        use super::super::Common;
+        use avian3d::prelude::*;
+        use bevy::prelude::*;
+        use spacerobo_commons::Bullet;
+
+        #[test]
+        fn shoot_common_bullet() {
+            // Create app with MinimalPlugins and AssetPlugin, I use MinimalPlugin because there hasn't to use DefaultPlugins (No GUI).
+            // I also use two resources; `Assets<Mesh>` and `Assets<StandardMaterial>`.
+            let mut app = App::new();
+            app.add_plugins((MinimalPlugins, AssetPlugin::default()));
+            app.init_resource::<Assets<Mesh>>();
+            app.init_resource::<Assets<StandardMaterial>>();
+
+            // Passes origin place and force vector to fat struct which is derived from bevy::prelude::Resource trait.
+            let origin: Origin = Vec3::new(0.0, 0.0, 0.0).into();
+            let force: Force = Vec3::new(0.0, 0.0, 0.0).into();
+
+            // Insert them into the app.
+            app.insert_resource(origin);
+            app.insert_resource(force);
+            app.add_systems(Startup, shoot_once);
+
+            // Check the now status of app
+            // It should be empty in app
+            let world = app.world_mut();
+            world
+                .query_filtered::<(&Transform, &LinearVelocity, &AngularVelocity), With<Common>>()
+                .is_empty(world, world.last_change_tick(), world.read_change_tick());
+
+            // Update app
+            app.update();
+
+            // Check the now status of app
+            // There should be in app only one
+            let world = app.world_mut();
+            let mut common_bullet_query = world
+                .query_filtered::<(&Transform, &LinearVelocity, &AngularVelocity), With<Common>>();
+            let common_bullet_info = common_bullet_query
+                .single(world)
+                .expect("Common bullet should be spawned at once");
+
+            // Check the each parameters
+            assert_eq!(common_bullet_info.0.translation, Vec3::ZERO);
+            assert_eq!(**common_bullet_info.1, Vec3::ZERO);
+            assert_eq!(**common_bullet_info.2, Vec3::ZERO);
+
+            // Update app
+            app.update();
+
+            // Check the now status of app
+            // There should be in app only one
+            let world = app.world_mut();
+            let mut common_bullet_query = world
+                .query_filtered::<(&Transform, &LinearVelocity, &AngularVelocity), With<Common>>();
+            let common_bullet_info = common_bullet_query
+                .single(world)
+                .expect("Common bullet should be spawned at once");
+
+            // Check the each parameters
+            assert_eq!(common_bullet_info.0.translation, Vec3::ZERO);
+            assert_eq!(**common_bullet_info.1, Vec3::ZERO);
+            assert_eq!(**common_bullet_info.2, Vec3::ZERO);
+        }
+
+        fn shoot_once(
+            mut commands: Commands,
+            mut meshes: ResMut<Assets<Mesh>>,
+            mut materials: ResMut<Assets<StandardMaterial>>,
+            origin: Res<Origin>,
+            force: Res<Force>,
+        ) {
+            Common::shoot(
+                &mut commands,
+                &mut meshes,
+                &mut materials,
+                **origin,
+                **force,
+            );
+        }
+
+        #[derive(Deref, Resource)]
+        struct Origin(Vec3);
+
+        impl From<Vec3> for Origin {
+            fn from(from: Vec3) -> Origin {
+                Origin { 0: from }
+            }
+        }
+
+        #[derive(Deref, Resource)]
+        struct Force(Vec3);
+
+        impl From<Vec3> for Force {
+            fn from(from: Vec3) -> Force {
+                Force { 0: from }
+            }
+        }
     }
 }
