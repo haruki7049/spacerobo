1b01072a6fe2514bae3d814a35ac76a3a19ef421
haruki7049 <tontonkirikiri@gmail.com>
2026-05-28 09:49:35 +0900

    Update

--- crates/commons/src/controllable/mouse.rs
+++ crates/commons/src/controllable/mouse.rs
@@ -3,162 +3,134 @@ use crate::configs::GameConfigs;
 use avian3d::prelude::*;
 use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*};
 
+/// Flight camera rotation
+/// Mouse control
+fn flight_camera(
+    game_configs: GameConfigs,
+    accumulated_mouse_motion: AccumulatedMouseMotion,
+    transform: &mut Transform,
+    angular: &mut AngularVelocity,
+) {
+    if accumulated_mouse_motion.delta != Vec2::ZERO {
+        let delta = accumulated_mouse_motion.delta;
+
+        let x = -delta.x / 100.;
+        let y = -delta.y / 100.;
+        let mouse: Vec2 = Vec2::new(x, y);
+
+        let rotation: Quat = transform.rotation;
+
+        let mut velocity: Vec3 = Vec3::ZERO;
+
+        // Mouse X
+        {
+            let direction: Vec3 = if game_configs.player.mouse.x_reverse {
+                rotation * Vec3::NEG_Z
+            } else {
+                rotation * Vec3::Z
+            };
+
+            let x: f32 = mouse.x * direction.x;
+            let y: f32 = mouse.x * direction.y;
+            let z: f32 = mouse.x * direction.z;
+            let result: Vec3 = Vec3::new(x, y, z);
+
+            // Add roll thruster's info
+            velocity += result * game_configs.player.robo.thruster.force.roll;
+        }
+
+        // Mouse Y
+        {
+            let direction: Vec3 = if game_configs.player.mouse.y_reverse {
+                rotation * Vec3::NEG_X
+            } else {
+                rotation * Vec3::X
+            };
+
+            let x: f32 = mouse.y * direction.x;
+            let y: f32 = mouse.y * direction.y;
+            let z: f32 = mouse.y * direction.z;
+            let result: Vec3 = Vec3::new(x, y, z);
+
+            // Add pitch thruster's info
+            velocity += result * game_configs.player.robo.thruster.force.pitch;
+        }
+
+        angular.0 += velocity;
+    }
+}
+
+/// Normal camera rotation
+/// Mouse control
+fn normal_camera(
+    game_configs: GameConfigs,
+    accumulated_mouse_motion: AccumulatedMouseMotion,
+    transform: &mut Transform,
+    angular: &mut AngularVelocity,
+) {
+    if accumulated_mouse_motion.delta != Vec2::ZERO {
+        let delta = accumulated_mouse_motion.delta;
+
+        let x = -delta.x / 100.;
+        let y = -delta.y / 100.;
+        let mouse: Vec2 = Vec2::new(x, y);
+
+        let rotation: Quat = transform.rotation;
+
+        let mut velocity: Vec3 = Vec3::ZERO;
+
+        // Mouse X
+        {
+            let direction: Vec3 = if game_configs.player.mouse.x_reverse {
+                rotation * Vec3::NEG_Y
+            } else {
+                rotation * Vec3::Y
+            };
+
+            let x: f32 = mouse.x * direction.x;
+            let y: f32 = mouse.x * direction.y;
+            let z: f32 = mouse.x * direction.z;
+            let result: Vec3 = Vec3::new(x, y, z);
+
+            // Add yaw thruster's info
+            velocity += result * game_configs.player.robo.thruster.force.yaw;
+        }
+
+        // Mouse Y
+        {
+            let direction: Vec3 = if game_configs.player.mouse.y_reverse {
+                rotation * Vec3::NEG_X
+            } else {
+                rotation * Vec3::X
+            };
+
+            let x: f32 = mouse.y * direction.x;
+            let y: f32 = mouse.y * direction.y;
+            let z: f32 = mouse.y * direction.z;
+            let result: Vec3 = Vec3::new(x, y, z);
+
+            // Add pitch thruster's info
+            velocity += result * game_configs.player.robo.thruster.force.pitch;
+        }
+
+        angular.0 += velocity;
+    }
+}
+
 pub fn update_system(
     mut query: Query<(&mut Transform, &mut AngularVelocity), With<Controllable>>,
     game_configs: Res<GameConfigs>,
     accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
     mouse_button: Res<ButtonInput<MouseButton>>,
 ) {
-    for (transform, mut angular) in query.iter_mut() {
+    for (mut transform, mut angular) in query.iter_mut() {
+        let cfgs: GameConfigs = game_configs.clone();
+        let mouse_motion: AccumulatedMouseMotion = accumulated_mouse_motion.clone();
+
         if !mouse_button.pressed(MouseButton::Right) {
-            // Normal camera rotation
-            // Mouse control
-            if accumulated_mouse_motion.delta != Vec2::ZERO {
-                let delta = accumulated_mouse_motion.delta;
-
-                let x = -delta.x / 100.;
-                let y = -delta.y / 100.;
-                let mouse: Vec2 = Vec2::new(x, y);
-
-                let rotation: Quat = transform.rotation;
-
-                let mut velocity: Vec3 = Vec3::ZERO;
-
-                // Mouse X
-                {
-                    let direction: Vec3 = if game_configs.player.mouse.x_reverse {
-                        rotation * Vec3::NEG_Y
-                    } else {
-                        rotation * Vec3::Y
-                    };
-
-                    let x: f32 = mouse.x * direction.x;
-                    let y: f32 = mouse.x * direction.y;
-                    let z: f32 = mouse.x * direction.z;
-                    let result: Vec3 = Vec3::new(x, y, z);
-
-                    // Add yaw thruster's info
-                    velocity += result * game_configs.player.robo.thruster.force.yaw;
-                }
-
-                // Mouse Y
-                {
-                    let direction: Vec3 = if game_configs.player.mouse.y_reverse {
-                        rotation * Vec3::NEG_X
-                    } else {
-                        rotation * Vec3::X
-                    };
-
-                    let x: f32 = mouse.y * direction.x;
-                    let y: f32 = mouse.y * direction.y;
-                    let z: f32 = mouse.y * direction.z;
-                    let result: Vec3 = Vec3::new(x, y, z);
-
-                    // Add pitch thruster's info
-                    velocity += result * game_configs.player.robo.thruster.force.pitch;
-                }
-
-                angular.0 += velocity;
-            }
-            // Normal camera rotation
-            // Mouse control
-            if accumulated_mouse_motion.delta != Vec2::ZERO {
-                let delta = accumulated_mouse_motion.delta;
-
-                let x = -delta.x / 100.;
-                let y = -delta.y / 100.;
-                let mouse: Vec2 = Vec2::new(x, y);
-
-                let rotation: Quat = transform.rotation;
-
-                let mut velocity: Vec3 = Vec3::ZERO;
-
-                // Mouse X
-                {
-                    let direction: Vec3 = if game_configs.player.mouse.x_reverse {
-                        rotation * Vec3::NEG_Y
-                    } else {
-                        rotation * Vec3::Y
-                    };
-
-                    let x: f32 = mouse.x * direction.x;
-                    let y: f32 = mouse.x * direction.y;
-                    let z: f32 = mouse.x * direction.z;
-                    let result: Vec3 = Vec3::new(x, y, z);
-
-                    // Add yaw thruster's info
-                    velocity += result * game_configs.player.robo.thruster.force.yaw;
-                }
-
-                // Mouse Y
-                {
-                    let direction: Vec3 = if game_configs.player.mouse.y_reverse {
-                        rotation * Vec3::NEG_X
-                    } else {
-                        rotation * Vec3::X
-                    };
-
-                    let x: f32 = mouse.y * direction.x;
-                    let y: f32 = mouse.y * direction.y;
-                    let z: f32 = mouse.y * direction.z;
-                    let result: Vec3 = Vec3::new(x, y, z);
-
-                    // Add pitch thruster's info
-                    velocity += result * game_configs.player.robo.thruster.force.pitch;
-                }
-
-                angular.0 += velocity;
-            }
+            normal_camera(cfgs, mouse_motion, &mut transform, &mut angular);
         } else {
-            // Flight camera rotation
-            // Mouse control
-            if accumulated_mouse_motion.delta != Vec2::ZERO {
-                let delta = accumulated_mouse_motion.delta;
-
-                let x = -delta.x / 100.;
-                let y = -delta.y / 100.;
-                let mouse: Vec2 = Vec2::new(x, y);
-
-                let rotation: Quat = transform.rotation;
-
-                let mut velocity: Vec3 = Vec3::ZERO;
-
-                // Mouse X
-                {
-                    let direction: Vec3 = if game_configs.player.mouse.x_reverse {
-                        rotation * Vec3::NEG_Z
-                    } else {
-                        rotation * Vec3::Z
-                    };
-
-                    let x: f32 = mouse.x * direction.x;
-                    let y: f32 = mouse.x * direction.y;
-                    let z: f32 = mouse.x * direction.z;
-                    let result: Vec3 = Vec3::new(x, y, z);
-
-                    // Add roll thruster's info
-                    velocity += result * game_configs.player.robo.thruster.force.roll;
-                }
-
-                // Mouse Y
-                {
-                    let direction: Vec3 = if game_configs.player.mouse.y_reverse {
-                        rotation * Vec3::NEG_X
-                    } else {
-                        rotation * Vec3::X
-                    };
-
-                    let x: f32 = mouse.y * direction.x;
-                    let y: f32 = mouse.y * direction.y;
-                    let z: f32 = mouse.y * direction.z;
-                    let result: Vec3 = Vec3::new(x, y, z);
-
-                    // Add pitch thruster's info
-                    velocity += result * game_configs.player.robo.thruster.force.pitch;
-                }
-
-                angular.0 += velocity;
-            }
+            flight_camera(cfgs, mouse_motion, &mut transform, &mut angular);
         }
     }
 }
