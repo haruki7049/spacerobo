eee7122a840ed16def50e6b1e78801ce7da97637
haruki7049 <tontonkirikiri@gmail.com>
2026-06-03 01:10:49 +0900

    Update

--- crates/gun/src/gun.rs
+++ crates/gun/src/gun.rs
@@ -4,8 +4,10 @@ pub mod bullet;
 pub mod select_fire;
 
 use self::select_fire::SelectFire;
+use avian3d::prelude::*;
 use bevy::prelude::*;
-use spacerobo_commons::Weapon;
+use spacerobo_commons::{DeathMessage, Hp, Weapon};
+use spacerobo_target::Target;
 
 /// Gun component
 #[derive(Component)]
@@ -42,6 +44,8 @@ impl Weapon for Gun {
                         amount: 0.01,
                     },
                 }),
+                Collider::cylinder(0.125, 2.0),
+                CollisionEventsEnabled,
             ))
             // Spot light
             .with_child((
@@ -91,5 +95,39 @@ pub struct Muzzle;
 pub fn gun_cooling_system(mut gun: Query<&mut Gun>) {
     for mut gun in gun.iter_mut() {
         gun.interval.rest -= gun.interval.amount;
+    }
+}
+
+pub fn gun_melee_damage_system(
+    mut collision_event_reader: MessageReader<CollisionStart>,
+    mut death_message_writer: MessageWriter<DeathMessage>,
+    gun_query: Query<(), With<Gun>>,
+    mut target_query: Query<&mut Hp, With<Target>>,
+) {
+    for event in collision_event_reader.read() {
+        debug!("Collision!!");
+
+        let e1 = event.collider1;
+        let e2 = event.collider2;
+
+        // Check which entity is the target when the gun collides
+        let target_entity = if gun_query.contains(e1) {
+            e2
+        } else if gun_query.contains(e2) {
+            e1
+        } else {
+            continue;
+        };
+
+        const HUGE_DAMAGE: f32 = 20000.0;
+
+        if let Ok(mut hp) = target_query.get_mut(target_entity) {
+            // Decrease HP
+            hp.rest -= HUGE_DAMAGE;
+
+            if hp.rest <= 0. {
+                death_message_writer.write(DeathMessage::new(target_entity));
+            }
+        }
     }
 }
